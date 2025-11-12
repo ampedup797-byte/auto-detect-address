d0035aa4624f557c69c4a3e4c36a8de0d68ed23a
ampedup797-byte <ampedup797@gmail.com>
2025-11-12 02:38:20 -0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-
+import "./pasted-Shopify-COD-Order.js";
 const app = express();
 
 declare module 'http' {
